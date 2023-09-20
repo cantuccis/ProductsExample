648382a4ba7d7721cbc2a7801b53f7c028d87d8a
cantuccis <facuarancet97@gmail.com>
2023-09-19 21:48:22 -0300

    [REFACTOR] Validate positive price in price setter

--- Backend/Product.cs
+++ Backend/Product.cs
@@ -2,18 +2,29 @@ namespace Backend
 {
     public class Product
     {
+        private double price;
+
         public int Id { get; private set; }
         public string Name { get; private set; }
-        public double Price { get; private set; }
+        public double Price
+        {
+            get => price;
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Price cannot be negative");
+                }
+                
+                price = value;
+            }
+        }
         public Currency Currency { get; private set; }
         public int Quantity { get; private set; }
 
         public Product(int id, ProductData data)
         {
-            if(data.Price < 0) {
-                throw new ArgumentException("Price cannot be negative");
-            }
-
             Id = id;
             Name = data.Name;
             Price = data.Price;
