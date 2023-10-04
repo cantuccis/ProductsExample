fdde5f80ecf02066f2a9589398dd28fac97d09ab
Facundo Arancet <facuarancet97@gmail.com>
2023-10-04 02:36:38 -0300

    [RED] authentication tests

--- Backend.Test/AuthTest.cs
+++ Backend.Test/AuthTest.cs
@@ -10,9 +10,79 @@ namespace Backend.Test;
 public class AuthTest
 {
     [TestMethod]
-    void CreateAuthManagerTest()
+    public void CreateAuthManagerTest()
     {
-       // var auth = new AuthManager();
+       var auth = new AuthManager();
+       Assert.IsNotNull(auth);
+    }
 
+    [TestMethod]
+    public void SignUpTest()
+    {
+        // Arrange
+        var auth = new AuthManager();
+
+        // Act
+        auth.SignUp("someuser", "somepass");
+
+        // Assert
+        Assert.IsTrue(auth.ExistsUser("someuser"));
+    }
+
+    [TestMethod]
+    public void SignUpShouldFailIfUsernameIsAlreadyTakenTest()
+    {
+        // Arrange
+        var auth = new AuthManager();
+        auth.SignUp("someuser", "somepass");
+
+        // Act
+        var exception = Assert.ThrowsException<ArgumentException>(() => auth.SignUp("someuser", "pass1234"));
+
+        // Assert
+        Assert.AreEqual("Username already exists", exception.Message);
+    }
+
+    [TestMethod]
+    public void SignInTest()
+    {
+        // Arrange
+        var auth = new AuthManager();
+        auth.SignUp("someuser", "somepass");
+
+        // Act
+        var credentials = auth.SignIn("someuser", "somepass");
+
+        // Assert
+        Assert.IsNotNull(credentials);
+        Assert.AreEqual(credentials.Username);
+    }
+
+    [TestMethod]
+    public void SignInShouldFailIfUserDoesNotExistTest()
+    {
+        // Arrange
+        var auth = new AuthManager();
+        auth.SignUp("someuser", "somepass");
+
+        // Act
+        var exception = Assert.ThrowsException<ArgumentException>(() => auth.SignIn("someotheruser", "somepass"));
+
+        // Assert
+        Assert.AreEqual("Invalid username/password", exception.Message);
+    }
+
+    [TestMethod]
+    public void SignInShouldFailIfPasswordIsIncorrectTest()
+    {
+        // Arrange
+        var auth = new AuthManager();
+        auth.SignUp("someuser", "somepass");
+
+        // Act
+        var exception = Assert.ThrowsException<ArgumentException>(() => auth.SignIn("someuser", "notthepass"));
+
+        // Assert
+        Assert.AreEqual("Invalid username/password", exception.Message);
     }
 }
